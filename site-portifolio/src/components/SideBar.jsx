8374bc998e2b16b7026782cd227c7d9cc247ccb3
Mateus Walz <mateus.walz@gmail.com>
2023-04-25 09:39:26 -0300

    Implementa componente de barra lateral (#1)

---
+++ site-portifolio/src/components/SideBar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+import { menuItems } from "../utils/constants";
+
+function SideBar({ selectedMenuItem, setSelectedMenuItem }) {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+
+  return (
+    <div className={`sidebar ${collapsed ? "collapsed" : ""}`}>
+      <ul>
+        {menuItems.map((menuItem) => (
+          <button
+            className="menu-item-btn"
+            onClick={() => setSelectedMenuItem(menuItem.name)}
+            style={{
+              background: menuItem.name === selectedMenuItem && "#FC1503",
+            }}
+            key={menuItem.name}
+          >
+            <span
+              style={{
+                color: menuItem.name === selectedMenuItem ? "white" : "red",
+                marginRight: "15px",
+              }}
+            >
+              {menuItem.icon}
+            </span>
+            <span
+              className="button-color-text"
+              style={{
+                opacity: menuItem.name === selectedMenuItem ? "1" : "0.8",
+              }}
+            >
+              {menuItem.name}
+            </span>
+          </button>
+        ))}
+      </ul>
+
+      <div className="sidebar-footer collapse-btn" onClick={toggleCollapsed}>
+        {collapsed ? (
+          <BsFillArrowLeftCircleFill />
+        ) : (
+          <BsFillArrowRightCircleFill />
+        )}
+      </div>
+    </div>
+  );
+}
+export default SideBar;
---
+++ site-portifolio/src/utils/constants.js
@@ -0,0 +1,9 @@
+import { BsFillBookFill, BsFillFileEarmarkPostFill, BsFillClockFill } from 'react-icons/bs';
+import { SiAboutdotme } from 'react-icons/si';
+
+export const menuItems = [
+    { name: 'Sobre', icon: <SiAboutdotme />, },
+    { name: 'Skils', icon: <BsFillBookFill />, },
+    { name: 'Experiência', icon: <BsFillClockFill />, },
+    { name: 'Projetos', icon: <BsFillFileEarmarkPostFill />, },
+];
