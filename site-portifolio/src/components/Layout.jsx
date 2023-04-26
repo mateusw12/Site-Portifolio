358aa83c51cc387717004ffe14e3c86744b5e967
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-04-26 11:13:32 -0300

    Ajusta responsividade da página de projetos

--- site-portifolio/src/components/Layout.jsx
+++ site-portifolio/src/components/Layout.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import { useMediaQuery } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import { Sidebar } from "./index";
 
-const Layout = ({
-  children,
-  isSidebarCollapsed,
-  selectedMenuItem,
-  setSelectedMenuItem,
-}) => {
+const Layout = ({ children }) => {
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  const isMobile = useMediaQuery("(max-width:600px)");
+
+  useEffect(() => {
+    setIsMobileView(isMobile);
+  }, [isMobile]);
+
+  useEffect(() => {
+    const divElement = document.getElementById("children");
+    if (divElement) {
+      divElement.style.marginLeft = isSidebarCollapsed
+        ? isMobileView
+          ? "50px"
+          : "250px"
+        : isMobileView
+        ? "150px"
+        : "250px";
+    }
+  }, [isSidebarCollapsed, isMobileView]);
+
+  const handleSidebarCollapse = () => {
+    setIsSidebarCollapsed(!isSidebarCollapsed);
+  };
+
   return (
     <div className="image">
       <div style={{ display: "flex", height: "100vh" }}>
-        {!isSidebarCollapsed && <Sidebar />}
+        <Sidebar
+          isSidebarCollapsed={isSidebarCollapsed}
+          handleSidebarCollapse={handleSidebarCollapse}
+        />
         <div
+          id="children"
           style={{
             flex: 1,
             padding: "20px",
-            marginLeft: isSidebarCollapsed ? "80px" : "250px",
           }}
         >
           {children}
