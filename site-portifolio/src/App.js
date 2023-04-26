a34ccfb3b7024569086d73cc69f0b8a4f0e54445
Mateus Walz <mateus.walz@gmail.com>
2023-04-26 19:59:16 -0300

    Implementa página inicial (#18)

--- site-portifolio/src/App.js
+++ site-portifolio/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { About, Layout, Project, Skills } from './components';
+import { About, Home, Layout, Project, Skills } from './components';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -11,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Layout >
         <Routes>
+          <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
           <Route exact path="/skills" element={<Skills />} />
           <Route exact path="/projects" element={<Project />} />
---
+++ site-portifolio/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="home-content-title">
+        <h1>SEJA BEM VINDO AO MEU PORTIFÓLIO!!</h1>
+      </div>
+      <div className="home-content">
+        <h3>
+          FIQUE A VONTADE PARA EXPLORAR, ACESSE AS MÍDIAS SOCIAS PARA ENTRAR EM
+          CONTATO!!
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
