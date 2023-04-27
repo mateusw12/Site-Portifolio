a6042899fb1250983cb0b6eb090cac226de54620
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-04-26 21:34:27 -0300

    Ajusta responsividade na página home

--- site-portifolio/src/components/Home.jsx
+++ site-portifolio/src/components/Home.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { SidebarContext } from "../context/SidebarContext";
 import "../styles/Home.css";
 
 const Home = () => {
+  const { collapsed } = useContext(SidebarContext);
+
+  useEffect(() => {
+    // Aqui você pode usar o estado collapsed conforme necessário
+    const divElement = document.getElementById("home");
+
+    if (!divElement) return;
+    collapsed
+      ? divElement.classList.add("container")
+      : divElement.classList.remove("container");
+  }, [collapsed]);
+
   return (
-    <div className="home-container">
+    <div id="home" className="home-container">
       <div className="home-content-title">
         <h1>SEJA BEM VINDO AO MEU PORTIFÓLIO!!</h1>
       </div>
