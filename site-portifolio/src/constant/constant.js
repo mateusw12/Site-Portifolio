3b4da072c2aa098d98967e5e5f37c7a329480ec7
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-05-23 18:58:16 -0300

    Implementa nova constante de links de rede social

--- site-portifolio/src/constant/constant.js
+++ site-portifolio/src/constant/constant.js
@@ -17,28 +17,39 @@ export const menuItems = [
 
 export const socialMediaItems = [
     {
-        "value": "47 988334213",
-        "icon": "fa fa-phone-square",
-        "name": "Telefone"
+        value: "47 988334213",
+        icon: "fa fa-phone-square",
+        name: "Telefone"
     },
     {
-        "value": "mateus.walz@gmail.com",
-        "icon": "fa fa-envelope",
-        "name": "E-mail"
+        value: "mateus.walz@gmail.com",
+        icon: "fa fa-envelope",
+        name: "E-mail"
     },
     {
-        "value": "github.com/mateusw12",
-        "icon": "fa fa-github",
-        "name": "GitHub"
+        value: "github.com/mateusw12",
+        icon: "fa fa-github",
+        name: "GitHub"
     },
     {
-        "value": "linkedin.com/in/mateus-walz-b40866137/",
-        "icon": "fa fa-linkedin",
-        "name": "Linkedin"
+        value: "linkedin.com/in/mateus-walz-b40866137/",
+        icon: "fa fa-linkedin",
+        name: "Linkedin"
     },
     {
-        "value": "47 988226082",
-        "icon": "fa fa-whatsapp",
-        "name": "Whats app"
+        value: "47 988226082",
+        icon: "fa fa-whatsapp",
+        name: "Whats app"
     },
+]
+
+export const socialLinks = [
+    {
+        link:"https://github.com/mateusw12",
+        icon: "fa fa-github-square"
+    },
+    {
+        link: "https://www.linkedin.com/in/mateus-walz-b40866137/",
+        icon: "fa fa-linkedin-square"
+    }
 ]
