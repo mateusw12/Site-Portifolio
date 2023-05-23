b7da45bb47e84b70543179b6ec812221b880e82b
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-05-22 22:10:28 -0300

    Ajusta página de contatos Implementa página de contato

---
+++ site-portifolio/src/components/contact.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+import { socialMediaItems } from '../constant/constant';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Mateus Walz</h2>
+                        <img
+                            src="https://mitadmissions.org/wp-content/uploads/2018/07/avatar_5492.jpg"
+                            alt="Mateus Walz"
+                            style={{ height: '250px' }}
+                        />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
+                            A educação é a chave que abre as portas do conhecimento, transformando vidas e construindo um futuro melhor.
+                        </p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Entre em contato</h2>
+                        <hr />
+
+                        <div className="contact-list">
+                            <List>
+                                {
+                                    socialMediaItems.map(el => (
+                                        <ListItem>
+                                            <ListItemContent style={{ fontSize: '30px', fontFamily: 'anton' }}>
+                                                <i className={el.icon}> {el.value}</i>
+                                            </ListItemContent>
+                                        </ListItem>
+                                    ))
+                                }
+                            </List>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default Contact;
--- site-portifolio/src/constant/constant.js
+++ site-portifolio/src/constant/constant.js
@@ -14,3 +14,31 @@ export const menuItems = [
         "link": "/contact"
     },
 ]
+
+export const socialMediaItems = [
+    {
+        "value": "47 988334213",
+        "icon": "fa fa-phone-square",
+        "name": "Telefone"
+    },
+    {
+        "value": "mateus.walz@gmail.com",
+        "icon": "fa fa-envelope",
+        "name": "E-mail"
+    },
+    {
+        "value": "github.com/mateusw12",
+        "icon": "fa fa-github",
+        "name": "GitHub"
+    },
+    {
+        "value": "linkedin.com/in/mateus-walz-b40866137/",
+        "icon": "fa fa-linkedin",
+        "name": "Linkedin"
+    },
+    {
+        "value": "47 988226082",
+        "icon": "fa fa-whatsapp",
+        "name": "Whats app"
+    },
+]
