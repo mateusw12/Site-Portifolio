f4894967b912df5d2d45e6f04851ec5d0f55477b
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-05-22 22:03:10 -0300

    Implementa nova configuração de pagina

--- site-portifolio/src/App.js
+++ site-portifolio/src/App.js
@@ -1,24 +1,41 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { About, Content, Home, NotFound, Project, Skills } from "./components/index";
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
 import './App.css';
+import { Layout, Navigation, Header, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
+import { menuItems } from './constant/constant';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Content>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Content>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+        <Layout>
+          <Header className="header-color" title={<Link style={{ textDecoration: 'none', color: 'white' }} to="/">Home</Link>} scroll>
+            <Navigation>
+              {
+                menuItems.map(el =>
+                  <Link to={el.link}>{el.name}</Link>
+                )
+              }
+            </Navigation>
+          </Header>
+          <Drawer title={<Link style={{ textDecoration: 'none', color: 'black' }} to="/">Home</Link>}>
+            <Navigation>
+              {
+                menuItems.map(el =>
+                  <Link to={el.link}>{el.name}</Link>
+                )
+              }
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
