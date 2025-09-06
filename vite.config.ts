7e05269a2de9da1e0ac414a529222b7524afbc79
mateus.walz <mateus.walz@gmail.com>
2025-09-06 11:29:43 -0300

    habilita source map no vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'), // Configura o alias "@" para apontar para "src"
     },
   },
+  build: {
+    sourcemap: true,
+  },
 });
