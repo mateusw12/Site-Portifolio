caaf1bc93fb938aab922f76e91e2cdfc13ba4185
mateus.walz <mateus.walz@gmail.com>
2025-09-06 14:25:10 -0300

    adiciona animação ao ir a seção sobre

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,30 @@
 import { Button } from "@/components/ui/button";
 import { RiArrowDownDoubleFill } from "react-icons/ri";
+import { animate, motion } from "framer-motion";
 
 interface HomeProps {
   id: string;
 }
 
-export function Home(props: HomeProps) {
+export function Home({ id }: HomeProps) {
+
+  const handleScrollToAbout = () => {
+    const aboutSection = document.getElementById("about");
+    if (!aboutSection) return;
+
+    const top = aboutSection.offsetTop;
+
+    animate(window.scrollY, top, {
+      duration: 0.8,
+      ease: [0.25, 0.1, 0.25, 1], 
+      onUpdate(value) {
+        window.scrollTo(0, value);
+      },
+    });
+  };
+
   return (
-    <div id={props.id} className="bg-light-background dark:bg-dark-background">
+    <div id={id} className="bg-light-background dark:bg-dark-background">
       <div className="container mx-auto h-screen flex flex-col justify-center items-center gap-6 px-4 py-4 relative">
         <span className="text-lg sm:text-xl md:text-2xl text-light-text dark:text-dark-text animate-slide-up">
           hello world!
@@ -24,7 +41,17 @@ export function Home(props: HomeProps) {
           </a>
         </Button>
 
-        <RiArrowDownDoubleFill className="absolute bottom-5 text-4xl animate-bounce text-light-secondary dark:text-dark-primary" />
+        <motion.div
+          onClick={handleScrollToAbout}
+          className="absolute bottom-5 cursor-pointer"
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 0.9 }}
+          initial={{ y: 0 }}
+          animate={{ y: [0, -10, 0] }}
+          transition={{ repeat: Infinity, duration: 1.5 }}
+        >
+          <RiArrowDownDoubleFill className="text-4xl text-light-secondary dark:text-dark-primary" />
+        </motion.div>
       </div>
     </div>
   );
