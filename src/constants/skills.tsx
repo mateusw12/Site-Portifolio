7403145dc40c39e35b53b10d9867743c7fa7a5ba
mateus.walz <mateus.walz@gmail.com>
2025-09-06 14:08:30 -0300

    Atualiza skills

--- src/constants/skills.tsx
+++ src/constants/skills.tsx
@@ -1,25 +1,21 @@
-import {
-  FaAngular,
-  FaBootstrap,
-  FaGithub,
-  FaJava,
-  FaPython,
-  FaReact,
-} from "react-icons/fa";
-import { RiNextjsLine, RiTailwindCssFill } from "react-icons/ri";
+import { FaAngular, FaJava, FaPython, FaReact } from "react-icons/fa";
+import { BiLogoSpringBoot } from "react-icons/bi";
+import { RiNextjsLine } from "react-icons/ri";
 import { FaGitAlt } from "react-icons/fa";
 import { AiOutlineDotNet } from "react-icons/ai";
 import { ISkill } from "./interface";
+import { TbSql } from "react-icons/tb";
+import { SiNestjs } from "react-icons/si";
 
 export const SKILLS: ISkill[] = [
   { name: "Angular", icon: <FaAngular /> },
   { name: "React", icon: <FaReact /> },
   { name: "Next.js", icon: <RiNextjsLine /> },
   { name: "Java", icon: <FaJava /> },
+  { name: "Spring Boot", icon: <BiLogoSpringBoot /> },
   { name: "Python", icon: <FaPython /> },
   { name: ".NET", icon: <AiOutlineDotNet /> },
-  { name: "Bootstrap", icon: <FaBootstrap /> },
-  { name: "Tailwind", icon: <RiTailwindCssFill /> },
+  { name: "Nest Js", icon: <SiNestjs /> },
+  { name: "SQL", icon: <TbSql /> },
   { name: "Git", icon: <FaGitAlt /> },
-  { name: "GitHub", icon: <FaGithub /> },
 ];
